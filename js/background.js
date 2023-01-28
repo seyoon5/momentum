d34e81a9d0009efd1581e7d2e3ef2dca370281e6
seyoon5 <82998388+seyoon5@users.noreply.github.com>
2023-01-29 01:54:08 +0900

    sibal

--- js/background.js
+++ js/background.js
@@ -3,4 +3,4 @@ const images = ["img1.jpg", "img3.jpg", "img2.jpg"];
 const chosenImages = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.querySelector("body");
-bgImage.style.backgroundImage = `url(/images/${chosenImages})`;
+bgImage.style.backgroundImage = `url(./images/${chosenImages})`;
